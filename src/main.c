5529de3da50d9f21d503bd8f15ab34af4b3a2183
Wafffel <kristoferski.kw@gmail.com>
2025-12-24 15:37:14 +0100

    add help message

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  char *help_messge = "Uzycie: %s <plik_mapy> [opcje]\n"
+                      "Opcje:\n"
+                      "  -h, --help       Wyswietl ta pomoc\n"
+                      "  -p               liczba czastek (domyslnie 30)\n"
+                      "  -i               liczba iteracji (domyslnie 100)\n"
+                      "  -c               sciezka do pliku konfiguracyjnego\n"
+                      "  -n               co ktora iteracje zapis postepow (domyslnie 0 - brak zapisu)\n"; 
   return EXIT_SUCCESS;
 }
